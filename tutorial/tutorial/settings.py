38b20ec10d376ffb7efb07d7718e1e5798a8818b
Valentin Herrmann <valentin.herrmann@tum.de>
2025-04-24 14:40:13 +0200

    added my server ip to allowed_hosts

--- tutorial/tutorial/settings.py
+++ tutorial/tutorial/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['db.valentin-herrmann.com','127.0.0.1','localhost']
+ALLOWED_HOSTS = ['db.valentin-herrmann.com','127.0.0.1','localhost', '217.154.94.239']
 
 #SESSION_COOKIE_SECURE = True
 #CSRF_COOKIE_SECURE = True
