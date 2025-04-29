375615d1bb738b87c900b5d0ac116f23a3f868df
Valentin Herrmann <valentin.herrmann@tum.de>
2025-04-29 12:10:29 +0200

    path zip

--- tutorial/myapp/models.py
+++ tutorial/myapp/models.py
@@ -1,8 +1,5 @@
-import os
 from django.db import models
-
-from tutorial.settings import BASE_DIR
 
 class ZippedFolder(models.Model):
     name = models.CharField(max_length=255, primary_key=True)
-    zip_file = models.FileField(upload_to=os.path.join(BASE_DIR, 'static'))
+    zip_file = models.FileField(upload_to='/zip/')
