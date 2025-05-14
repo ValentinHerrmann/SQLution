85db06ae5890e631c6ff7228dcda0e5904bb363c
Valentin Herrmann <valentin.herrmann@tum.de>
2025-05-14 11:32:58 +0200

    very stupid path mistake

--- update_and_launch.sh
+++ update_and_launch.sh
@@ -7,7 +7,7 @@ python3 manage.py makemigrations
 python3 manage.py migrate
 python3 manage.py collectstatic --noinput
 
-rm /home/vale/github/django-tutorial/tutorial/last_launched.txt
-date '+%Y-%m-%d %H:%M:%S' > /home/vale/github/django-tutorial/tutorial/last_launched.txt
+rm ./last_launched.txt
+date '+%Y-%m-%d %H:%M:%S' > ./last_launched.txt
 
 nohup gunicorn --bind 0.0.0.0:8000 tutorial.wsgi &
