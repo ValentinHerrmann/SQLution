b46b2d0ba51c91b38d85968d492ddcb304b94420
Valentin Herrmann <accounts@valentin-herrmann.com>
2025-05-14 08:52:21 +0000

    added launch script

---
+++ update_and_launch.sh
@@ -0,0 +1,10 @@
+pkill gunicorn
+source ../../bin/activate
+git pull
+pip install -r requirements.txt
+cd tutorial
+python3 manage.py makemigrations
+python3 manage.py migrate
+python3 manage.py collectstatic --noinput
+nohup gunicorn --bind 0.0.0.0:8000 tutorial.wsgi &
+
