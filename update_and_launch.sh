9a6f81761e3bcd7721eaa431ebf971549b6546e3
Valentin Herrmann <valentin.herrmann@tum.de>
2025-05-14 11:44:49 +0200

    inserted timezone

--- update_and_launch.sh
+++ update_and_launch.sh
@@ -8,6 +8,6 @@ python3 manage.py migrate
 python3 manage.py collectstatic --noinput
 
 rm ./last_launched.txt
-date '+%Y-%m-%d %H:%M:%S' > ./last_launched.txt
+TZ=Europe/Berlin date '+%Y-%m-%d %H:%M:%S' > ./last_launched.txt
 
 nohup gunicorn --bind 0.0.0.0:8000 tutorial.wsgi &
