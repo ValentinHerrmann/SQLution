bc43bb052e1e66502e718c3761ff7d7779c2f993
Valentin Herrmann <accounts@valentin-herrmann.com>
2025-10-23 15:25:46 +0200

    Reverted last commit

--- launch.sh
+++ launch.sh
@@ -1,5 +1,10 @@
+pkill gunicorn
 source ../../bin/activate
+pip install -r requirements.txt
 cd tutorial
+python3 manage.py makemigrations --merge
+python3 manage.py migrate
+python3 manage.py collectstatic --noinput
 
 rm ./last_launched.txt
 TZ=Europe/Berlin date '+%Y-%m-%d %H:%M:%S TZ=Eu/Ber' > ./last_launched.txt
