2eb9731ed13c41312c75904bdddfe296925e278a
Valentin Herrmann <accounts@valentin-herrmann.com>
2025-12-02 19:04:14 +0100

    lets see if the problem is fixed

--- launch.sh
+++ launch.sh
@@ -9,7 +9,7 @@ mkdir -p tutorial/user_databases
 
 docker compose -f compose.yaml down
 docker compose -f compose.yaml pull
-docker compose -f compose.yaml up -d --no-build --remove-orphans
+SQLUTION_IMAGE="${SQLUTION_IMAGE:-sqlution:local}" docker compose -f compose.yaml up -d --no-build --remove-orphans
 
 rm -f ./last_launched.txt
 TZ=Europe/Berlin date '+%Y-%m-%d %H:%M:%S TZ=Eu/Ber' > ./last_launched.txt
