144b7448c6422bfa76552bb4c95e0d010c8f2730
Valentin Herrmann <accounts@valentin-herrmann.com>
2025-12-02 18:44:52 +0100

    try with container reg

--- launch.sh
+++ launch.sh
@@ -8,8 +8,8 @@ cd "$REPO_ROOT"
 mkdir -p tutorial/user_databases
 
 docker compose -f compose.yaml down
-docker compose -f compose.yaml build --pull
-docker compose -f compose.yaml up -d --remove-orphans
+docker compose -f compose.yaml pull
+docker compose -f compose.yaml up -d --no-build --remove-orphans
 
 rm -f ./last_launched.txt
 TZ=Europe/Berlin date '+%Y-%m-%d %H:%M:%S TZ=Eu/Ber' > ./last_launched.txt
