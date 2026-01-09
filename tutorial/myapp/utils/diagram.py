4127afa24461006e8351cf4b18c55cc052a74c27
Valentin Herrmann <accounts@valentin-herrmann.com>
2026-01-09 15:55:29 +0100

    refactoring

--- tutorial/myapp/utils/diagram.py
+++ tutorial/myapp/utils/diagram.py
@@ -1,8 +1,10 @@
 import json
 
 
-from myapp.utils.sqlite_connector import *
-from myapp.utils.utils import extract_tables, format_sql
+from myapp.utils.utils import format_sql
+from myapp.utils.json_to_sql import extract_tables
+from myapp.utils.directories import get_user_directory
+from myapp.utils.sqlite_connector import create_db
 
 
 
@@ -16,8 +18,6 @@ def load_json(json_bytes, username):
 
         with open(get_user_directory(username)+'/_CreateDB.sql_', "w") as f:
             f.write(sql_output)
-        with open(get_user_directory(username)+'/model.json', "wb+") as f:
-            f.write(json_bytes)
         create_db(sql_output, username)  # Call the function to execute SQL statements
 
     except Exception as e:
