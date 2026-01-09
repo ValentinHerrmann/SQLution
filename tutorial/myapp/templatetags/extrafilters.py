0a6423f1d71eacbdffa8ed04f2e0ffc6efb2994e
Valentin Herrmann <accounts@valentin-herrmann.com>
2026-01-09 18:55:57 +0100

    fixed pr version recognition

--- tutorial/myapp/templatetags/extrafilters.py
+++ tutorial/myapp/templatetags/extrafilters.py
@@ -30,8 +30,8 @@ def version_helper():
     try:
         with open(version_file, 'r') as f:
             v = f.read().strip()
-            if "merge/" in v:
-                v = v.replace("merge/", "")
+            if "/merge" in v:
+                v = v.replace("/merge", "")
                 url = f"https://github.com/ValentinHerrmann/SQLution/pull/{v}"
                 version = f"PR #{v}"
             elif re.match(r'^\d+(\.\d+)?(\.\d+)?$', v):
